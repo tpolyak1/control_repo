68420085d80f6776951b4692423ad6353fbc441b
tpolyak1 <50884382+tpolyak1@users.noreply.github.com>
2019-05-24 08:46:21 -0500

    Update ssh_server.pp

--- site/profile/manifests/ssh_server.pp
+++ site/profile/manifests/ssh_server.pp
@@ -10,6 +10,6 @@ class profile::ssh_server {
 		ensure => present,
 		user   => 'root',
 		type   => 'ssh-rsa',
-		key    => '',
+		key    => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQC6ltScg9rKADdYKM0MWnW9zD72tKZuzK2qSisg6rTf5/W5RrZmr7NUmmkAeIWzfl9rxyfGuZ5r377ZWFYd7b0Be5kVbhzJoLbIU8HcKai+vI4GfSRptrZSpyXNpHYluZT0dkkCCsnOP6inJWyD8E5HCOun5VdRtfTWHHaOEPOieRyfJgUaMoMxVjHTyJ3vOQrtIZ5S6s9vk0ndQ/uT6P+xEf2yuRVwoPIZgSL7K/ZJ4uRR8m1P1nJSBBqakVHsPSLIZ2gAveKcDuG+RxiDKNrNurDQuS2+y2V/tCxttQ93r6rz4NwVmHHJu78ctgZ99+zzvpP+Tlczd/1Dt9p61EL7',
 	}  
 }
